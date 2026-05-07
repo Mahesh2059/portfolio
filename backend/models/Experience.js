a992c8d0cadf205a1dc2a3503e0efa7b5cd9741b
Mahesh devekota <maheshdevkota45@gmail.com>
2026-05-07 12:42:03 +0545

    update

--- backend/models/Experience.js
+++ backend/models/Experience.js
@@ -4,7 +4,7 @@ const ExperienceSchema = new mongoose.Schema({
     title: String,
     company: String,
     duration: String,
-    description: [String] // This is an array for your bullet points
+    description: [String] 
 });
 
 module.exports = mongoose.model('Experience', ExperienceSchema);
